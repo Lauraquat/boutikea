45e18b86d591fae70588de06d305d2d466aa435a
Thomas Dhollande <dhollande.thomas@gmail.com>
2021-11-27 21:57:23 +0100

    slider accueil

---
+++ assets/js/thomas.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+    addHomeSliderNav();
+
+    /**
+     * Move the slider to the desired slide.
+     * Change the color of the current link in the slider navigation bar.
+     * Remove the class "td_home-slider__slide--current" from the old current slide and add it to the new one.
+     * Remove the class "td_home-slider__nav__link--current" from the old current link in the slider navigation bar and add it to the new one.
+     */
+    $(document).on('click', 'li.td_home-slider__nav__link', function () {
+        let home_slider_slide_current = $('.td_home-slider__slide--current');
+        let home_slider_nav_link_current = $('.td_home-slider__nav__link--current');
+        let this_home_slider_link_data = $(this).data('home-slider__link');
+        let new_home_slider_slide_current = $('[data-home-slider_slide="' + this_home_slider_link_data + '"]');
+        let home_slider_width_move = (this_home_slider_link_data - 1) * 100;
+
+        $('main').animate({ 'right': home_slider_width_move + '%' }, 1200);
+        home_slider_slide_current.removeClass('td_home-slider__slide--current');
+        home_slider_nav_link_current.removeClass('td_home-slider__nav__link--current');
+        new_home_slider_slide_current.addClass('td_home-slider__slide--current');
+        $(this).addClass('td_home-slider__nav__link--current');
+    });
+
+
+    /**
+     * Add the home slider navigation bar.
+     */
+    function addHomeSliderNav() {
+        let home_sliders = $(".td_home-slider__container > div");
+        let count_home_sliders = home_sliders.length;
+
+        if (count_home_sliders > 1) {
+            $('<ul class="td_home-slider__nav w-100 position-fixed bottom-0 start-50"></ul>').appendTo('main');
+            let home_slider_nav = $("ul.td_home-slider__nav");
+
+            for (let i = 1; i <= count_home_sliders; i++) {
+                $('<li class="td_home-slider__nav__link" data-home-slider__link = ' + i + '><i class="fas fa-circle"></i></li>').appendTo(home_slider_nav);
+            }
+
+            $('.td_home-slider__nav__link').first().addClass('td_home-slider__nav__link--current');
+        }
+    }
+});

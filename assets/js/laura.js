83cd765db5a3d2a27f771b1644a2695c4b829451
Lauraquat <lauraquat@gmail.com>
2021-12-09 14:50:11 +0100

    fix affichage champ produit

--- assets/js/laura.js
+++ assets/js/laura.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-    
+    /*lien entre produits et page contact*/
+    $('.lq_link-more').click(function() {
+        var value =  $(this).attr("name");
+        $('#productValue').val(value);
+        
+        /*Affiche le champ produit dans le formulaire de contact*/
+        $('#productField').removeClass("d-none");
+    });
+
 });
--- assets/js/script.js
+++ assets/js/script.js
@@ -100,7 +100,8 @@ $(document).ready(function () {
     $('.lq_link-more').click(function() {
         var value =  $(this).attr("name");
         $('#productValue').val(value);
-    })
-
-    $('#productField').removeClass("d-none");
+        
+        /*Affiche le champ produit dans le formulaire de contact*/
+        $('#productField').removeClass("d-none");
+    });
 });

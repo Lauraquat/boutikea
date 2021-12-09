eac4e6731fe91af24a59918ead5c6a91015a5292
Lea SAGNIER <lea.sagnier@vinci-energies.net>
2021-12-09 15:27:55 +0100

    btn darkmode

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).ready(function() {
     var current_page = $(".current_page");
     var current_page_id = "#" + current_page.attr("id");
 
@@ -13,7 +13,7 @@ $(document).ready(function () {
      * Remove the class "td_home-slider__slide--current" from the old current slide and add it to the new one.
      * Remove the class "td_home-slider__nav__link--current" from the old current link in the slider navigation bar and add it to the new one.
      */
-    $(document).on('click', 'li.td_home-slider__nav__link', function () {
+    $(document).on('click', 'li.td_home-slider__nav__link', function() {
         let home_slider_slide_current = $('.td_home-slider__slide--current');
         let home_slider_nav_link_current = $('.td_home-slider__nav__link--current');
         let this_home_slider_link_data = $(this).data('home-slider__link');
@@ -27,7 +27,7 @@ $(document).ready(function () {
         $(this).addClass('td_home-slider__nav__link--current');
     });
 
-    $(document).on('click', 'a', function (e) {
+    $(document).on('click', 'a', function(e) {
         let hrefLink = $(this).attr("href");
 
         if (hrefLink != current_page_id) {
@@ -98,10 +98,32 @@ $(document).ready(function () {
 
     /*lien entre produits et page contact*/
     $('.lq_link-more').click(function() {
-        var value =  $(this).attr("name");
+        var value = $(this).attr("name");
         $('#productValue').val(value);
-        
+
         /*Affiche le champ produit dans le formulaire de contact*/
         $('#productField').removeClass("d-none");
     });
 });
+
+$(document).ready(function() {
+    $('.ls_checkbox').on('change', function() {
+        var is_it_checked = $(this).prop("checked"); //check the checkbox is checked or not
+        if (is_it_checked == true) {
+            $('.lq_product-details, .navbar').css('background-color', '#3a3a3a');
+            $('.navbar a, .navbar h2').css('color', 'white');
+            $('#quiSommesNous, #quiSommesNous h2, #quiSommesNous h3').css('color', 'white');
+            $('#boutique h2, #boutique, #boutique a').css('color', 'white');
+            $('.td_home-slider__slide--1,.td_home-slider__slide--2,.td_home-slider__slide--3').css('filter', 'grayscale(1)')
+            $(document.body, '.navbar').css('background-color', 'var(--noir)');
+        } else {
+            $('.lq_product-details').removeAttr('style');
+            $('.navbar').removeAttr('style');
+            $('.navbar a, .navbar h2').removeAttr('style');
+            $('#quiSommesNous, #quiSommesNous h2, #quiSommesNous h3').removeAttr('style');
+            $('#boutique h2, #boutique, #boutique a').removeAttr('style');
+            $('.td_home-slider__slide--1,.td_home-slider__slide--2,.td_home-slider__slide--3').removeAttr('style');
+            $(document.body).removeAttr('style');
+        }
+    })
+})

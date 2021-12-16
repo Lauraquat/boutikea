b70651c4499df4527d860f178088fd1cd4ad3f48
Thomas Dhollande <dhollande.thomas@gmail.com>
2021-12-16 12:25:56 +0100

    animation boutique

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
     var home = $("#home");
     var about = $("#quiSommesNous");
     var boutique = $("#boutique");
@@ -20,7 +20,7 @@ $(document).ready(function() {
      * Remove the class "td_home-slider__slide--current" from the old current slide and add it to the new one.
      * Remove the class "td_home-slider__nav__link--current" from the old current link in the slider navigation bar and add it to the new one.
      */
-    $(document).on('click', 'li.td_home-slider__nav__link', function() {
+    $(document).on('click', 'li.td_home-slider__nav__link', function () {
         let home_slider_slide_current = $('.td_home-slider__slide--current');
         let home_slider_nav_link_current = $('.td_home-slider__nav__link--current');
         let this_home_slider_link_data = $(this).data('home-slider__link');
@@ -67,7 +67,7 @@ $(document).ready(function() {
     /**
      * Transmet le href de la page sur laquelle on veut aller ainsi que l'id de la page courante à la fonction de transition animée
      */
-    $(document).on('click', 'a', function(e) {
+    $(document).on('click', 'a', function (e) {
         let hrefLink = $(this).attr("href");
         if (hrefLink != current_page_id) {
             animatedTransition(hrefLink, current_page, current_page_id, current_page_boutique, current_page_boutique_id);
@@ -83,23 +83,23 @@ $(document).ready(function() {
                 home.fadeOut(1000);
             } else if (currentPageId == "#boutique") {
                 boutique.css({ left: "100%" });
-                setTimeout(function() {
+                setTimeout(function () {
                     boutique.hide();
                 }, 1000);
             } else if (currentPageId == "#contact") {
                 contact.css({ right: "100%" });
                 contact.css({ transform: "rotate(50deg)" });
-                setTimeout(function() {
+                setTimeout(function () {
                     contact.removeClass("row");
                     contact.addClass("display-none");
                 }, 1000);
             }
 
-            setTimeout(function() {
+            setTimeout(function () {
                 about.css({ transform: "scale(0)" });
                 about.removeClass("display-none").addClass("display-flex");
             }, 1000);
-            setTimeout(function() {
+            setTimeout(function () {
                 about.css({ transform: "scale(1)" });
             }, 1100);
 
@@ -110,24 +110,24 @@ $(document).ready(function() {
         } else if (targetId == "#home") {
             if (currentPageId == "#quiSommesNous") {
                 about.css({ transform: "scale(0)" });
-                setTimeout(function() {
+                setTimeout(function () {
                     about.removeClass("display-flex").addClass("display-none");
                 }, 1000);
             } else if (currentPageId == "#boutique") {
                 boutique.css({ left: "100%" });
-                setTimeout(function() {
+                setTimeout(function () {
                     boutique.hide();
                 }, 1000);
             } else if (currentPageId == "#contact") {
                 contact.css({ right: "100%" });
                 contact.css({ transform: "rotate(50deg)" });
-                setTimeout(function() {
+                setTimeout(function () {
                     contact.removeClass("row");
                     contact.addClass("display-none");
                 }, 1000);
             }
 
-            setTimeout(function() {
+            setTimeout(function () {
                 home.fadeIn(500);
                 about.removeClass("display-flex").addClass("display-none");
             }, 1000);
@@ -141,23 +141,23 @@ $(document).ready(function() {
                 home.fadeOut(1000);
             } else if (currentPageId == "#quiSommesNous") {
                 about.css({ transform: "scale(0)" });
-                setTimeout(function() {
+                setTimeout(function () {
                     about.removeClass("display-flex").addClass("display-none");
                 }, 1000);
             } else if (currentPageId == "#contact") {
                 contact.css({ right: "100%" });
                 contact.css({ transform: "rotate(50deg)" });
-                setTimeout(function() {
+                setTimeout(function () {
                     contact.removeClass("row");
                     contact.addClass("display-none");
                 }, 1000);
             }
 
-            setTimeout(function() {
+            setTimeout(function () {
                 boutique.css({ left: "100%" });
                 boutique.show();
             }, 1000);
-            setTimeout(function() {
+            setTimeout(function () {
                 boutique.css({ left: "0" });
             }, 1100);
 
@@ -170,23 +170,23 @@ $(document).ready(function() {
                 home.fadeOut(1000);
             } else if (currentPageId == "#boutique") {
                 boutique.css({ left: "100%" });
-                setTimeout(function() {
+                setTimeout(function () {
                     boutique.hide();
                 }, 1000);
             } else if (currentPageId == "#quiSommesNous") {
                 about.css({ transform: "scale(0)" });
-                setTimeout(function() {
+                setTimeout(function () {
                     about.removeClass("display-flex").addClass("display-none");
                 }, 1000);
             }
 
-            setTimeout(function() {
+            setTimeout(function () {
                 contact.css({ right: "100%" });
                 contact.css({ transform: "rotate(-50deg)" });
                 contact.removeClass("display-none");
                 contact.addClass("row");
             }, 1000);
-            setTimeout(function() {
+            setTimeout(function () {
                 contact.css({ right: "0" });
                 contact.css({ transform: "rotate(0)" });
             }, 1100);
@@ -197,15 +197,30 @@ $(document).ready(function() {
             current_page_id = targetId;
         }
 
-        if (targetId == "#lq_product2") {
-            if (currentPageBoutiqueId == "#lq_product1") {
-                page_boutique_1.css({ top: "100%" });
+        if (targetId == "#lq_product1" || targetId == "#lq_product2" || targetId == "#lq_product3") {
+            if (currentPageBoutiqueId != targetId) {
+                $(`${targetId}`).css({ opacity: "0" });
+                currentPageBoutique.css({ opacity: "0" });
+                setTimeout(function () {
+                    currentPageBoutique.removeClass("d-flex").addClass("display-none");
+                    $(`${targetId}`).removeClass("display-none").addClass("d-flex");
+                }, 1000);
+                setTimeout(function () {
+                    currentPageBoutique.removeClass("d-flex");
+                    currentPageBoutique.addClass("display-none");
+                    $(`${targetId}`).css({ opacity: "1" });
+                }, 1100);
+
+                // Nouvelle page courante
+                $("#main-container").attr("data-current_page", targetId);
+                current_page_boutique = $(`${targetId}`);
+                current_page_boutique_id = targetId;
             }
         }
     }
 
     /*lien entre produits et page contact*/
-    $('.lq_link-more').click(function() {
+    $('.lq_link-more').click(function () {
         var value = $(this).attr("name");
         $('#productValue').val(value);
 
@@ -238,26 +253,26 @@ $(document).ready(function() {
     });
 
     /*Appel des notifications + réinitialisation du formulaire*/
-    $('#send').click(function(e) {
+    $('#send').click(function (e) {
         e.preventDefault();
         $('#contactForm').trigger('reset');
         $("#successText").text('Message bien envoyé !');
         $("#successNotification").jqxNotification("open");
     });
 
-    $('#return').click(function() {
+    $('#return').click(function () {
         $('#contactForm').trigger('reset');
         $("#errorText").text("Message non envoyé");
         $("#errorNotification").jqxNotification("open");
     });
     /*notif validation modal*/
-    $('#validInscription').click(function() {
+    $('#validInscription').click(function () {
         $("#successText").text('Inscription effectuée');
         $("#successNotification").jqxNotification("open");
     });
 
     /*dark mode*/
-    $('.ls_checkbox').on('change', function() {
+    $('.ls_checkbox').on('change', function () {
         var is_it_checked = $(this).prop("checked"); //check the checkbox is checked or not
         if (is_it_checked == true) {
             $('.lq_product-details, .navbar, .form-control, .modal-content').css('background-color', '#3a3a3a');
@@ -278,30 +293,28 @@ $(document).ready(function() {
     });
 
 
-
-
     //Affichage lightbox
-    $(".lq_img").click(function() {
+    $(".lq_img").click(function () {
         var img = $(this).attr('src');
         var imgLightbox = img.replace("lq_img--petites", "lq_img--grandes");
         $(".lq_img--lightbox").html("<img src='" + imgLightbox + "'>");
         $(".lq_img--lightbox").fadeIn("slow").css("display", "flex");
     });
 
-    $(".lq_img--lightbox").click(function() {
+    $(".lq_img--lightbox").click(function () {
         $(".lq_img--lightbox").fadeOut("fast");
     });
 
     /*modal*/
     var modal_affichee = "yes";
-    $("body").mouseleave(function() {
+    $("body").mouseleave(function () {
         if (modal_affichee == "yes") {
             console.log(modal_affichee);
             $('#inscriptionNewL').modal('show');
-            $('#annulerInscription, #validInscription').click(function() {
+            $('#annulerInscription, #validInscription').click(function () {
                 $('#inscriptionNewL').modal('hide');
             });
-            $('#validInscription').click(function(e) {
+            $('#validInscription').click(function (e) {
                 e.preventDefault();
                 modal_affichee = "no";
                 $('#inscriptionNewL').modal('hide');
@@ -313,17 +326,17 @@ $(document).ready(function() {
     var euro = $('#euro');
     var yen = $('#yen');
 
-    $(dollar).click(function() {
+    $(dollar).click(function () {
         $(".price1").replaceWith("<p class='title price1'>224,67$</p>");
         $(".price2").replaceWith("<p class='title price2'>450,47$</p>");
         $(".price3").replaceWith("<p class='title price3'>563,37$</p>");
     });
-    $(euro).click(function() {
+    $(euro).click(function () {
         $(".price1").replaceWith("<p class='title price1'>199€</p>");
         $(".price2").replaceWith("<p class='title price2'>399€</p>");
         $(".price3").replaceWith("<p class='title price3'>499€</p>");
     });
-    $(yen).click(function() {
+    $(yen).click(function () {
         $(".price1").replaceWith("<p class='title price1'>25617,34¥</p>");
         $(".price2").replaceWith("<p class='title price2'>51363,41¥</p>");
         $(".price3").replaceWith("<p class='title price3'>64236,45¥</p>");

76980b1c4b13ff69194332a6c4d814781b987c21
Lea SAGNIER <lea.sagnier@vinci-energies.net>
2021-12-16 10:29:51 +0100

    modal+monnaie

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).ready(function() {
     var home = $("#home");
     var about = $("#quiSommesNous");
     var boutique = $("#boutique");
@@ -20,7 +20,7 @@ $(document).ready(function () {
      * Remove the class "td_home-slider__slide--current" from the old current slide and add it to the new one.
      * Remove the class "td_home-slider__nav__link--current" from the old current link in the slider navigation bar and add it to the new one.
      */
-    $(document).on('click', 'li.td_home-slider__nav__link', function () {
+    $(document).on('click', 'li.td_home-slider__nav__link', function() {
         let home_slider_slide_current = $('.td_home-slider__slide--current');
         let home_slider_nav_link_current = $('.td_home-slider__nav__link--current');
         let this_home_slider_link_data = $(this).data('home-slider__link');
@@ -65,9 +65,9 @@ $(document).ready(function () {
 
 
     /**
-    * Transmet le href de la page sur laquelle on veut aller ainsi que l'id de la page courante à la fonction de transition animée
-    */
-    $(document).on('click', 'a', function (e) {
+     * Transmet le href de la page sur laquelle on veut aller ainsi que l'id de la page courante à la fonction de transition animée
+     */
+    $(document).on('click', 'a', function(e) {
         let hrefLink = $(this).attr("href");
         if (hrefLink != current_page_id) {
             animatedTransition(hrefLink, current_page, current_page_id, current_page_boutique, current_page_boutique_id);
@@ -81,27 +81,25 @@ $(document).ready(function () {
         if (targetId == "#quiSommesNous") {
             if (currentPageId == "#home") {
                 home.fadeOut(1000);
-            }
-            else if (currentPageId == "#boutique") {
+            } else if (currentPageId == "#boutique") {
                 boutique.css({ left: "100%" });
-                setTimeout(function () {
+                setTimeout(function() {
                     boutique.hide();
                 }, 1000);
-            }
-            else if (currentPageId == "#contact") {
+            } else if (currentPageId == "#contact") {
                 contact.css({ right: "100%" });
                 contact.css({ transform: "rotate(50deg)" });
-                setTimeout(function () {
+                setTimeout(function() {
                     contact.removeClass("row");
                     contact.addClass("display-none");
                 }, 1000);
             }
 
-            setTimeout(function () {
+            setTimeout(function() {
                 about.css({ transform: "scale(0)" });
                 about.removeClass("display-none").addClass("display-flex");
             }, 1000);
-            setTimeout(function () {
+            setTimeout(function() {
                 about.css({ transform: "scale(1)" });
             }, 1100);
 
@@ -109,31 +107,27 @@ $(document).ready(function () {
             $("#main-container").attr("data-current_page", targetId);
             current_page = $(`${targetId}`);
             current_page_id = targetId;
-        }
-
-        else if (targetId == "#home") {
+        } else if (targetId == "#home") {
             if (currentPageId == "#quiSommesNous") {
                 about.css({ transform: "scale(0)" });
-                setTimeout(function () {
+                setTimeout(function() {
                     about.removeClass("display-flex").addClass("display-none");
                 }, 1000);
-            }
-            else if (currentPageId == "#boutique") {
+            } else if (currentPageId == "#boutique") {
                 boutique.css({ left: "100%" });
-                setTimeout(function () {
+                setTimeout(function() {
                     boutique.hide();
                 }, 1000);
-            }
-            else if (currentPageId == "#contact") {
+            } else if (currentPageId == "#contact") {
                 contact.css({ right: "100%" });
                 contact.css({ transform: "rotate(50deg)" });
-                setTimeout(function () {
+                setTimeout(function() {
                     contact.removeClass("row");
                     contact.addClass("display-none");
                 }, 1000);
             }
 
-            setTimeout(function () {
+            setTimeout(function() {
                 home.fadeIn(500);
                 about.removeClass("display-flex").addClass("display-none");
             }, 1000);
@@ -142,32 +136,28 @@ $(document).ready(function () {
             $("#main-container").attr("data-current_page", targetId);
             current_page = $(`${targetId}`);
             current_page_id = targetId;
-        }
-
-        else if (targetId == "#boutique") {
+        } else if (targetId == "#boutique") {
             if (currentPageId == "#home") {
                 home.fadeOut(1000);
-            }
-            else if (currentPageId == "#quiSommesNous") {
+            } else if (currentPageId == "#quiSommesNous") {
                 about.css({ transform: "scale(0)" });
-                setTimeout(function () {
+                setTimeout(function() {
                     about.removeClass("display-flex").addClass("display-none");
                 }, 1000);
-            }
-            else if (currentPageId == "#contact") {
+            } else if (currentPageId == "#contact") {
                 contact.css({ right: "100%" });
                 contact.css({ transform: "rotate(50deg)" });
-                setTimeout(function () {
+                setTimeout(function() {
                     contact.removeClass("row");
                     contact.addClass("display-none");
                 }, 1000);
             }
 
-            setTimeout(function () {
+            setTimeout(function() {
                 boutique.css({ left: "100%" });
                 boutique.show();
             }, 1000);
-            setTimeout(function () {
+            setTimeout(function() {
                 boutique.css({ left: "0" });
             }, 1100);
 
@@ -175,32 +165,28 @@ $(document).ready(function () {
             $("#main-container").attr("data-current_page", targetId);
             current_page = $(`${targetId}`);
             current_page_id = targetId;
-        }
-
-        else if (targetId == "#contact") {
+        } else if (targetId == "#contact") {
             if (currentPageId == "#home") {
                 home.fadeOut(1000);
-            }
-            else if (currentPageId == "#boutique") {
+            } else if (currentPageId == "#boutique") {
                 boutique.css({ left: "100%" });
-                setTimeout(function () {
+                setTimeout(function() {
                     boutique.hide();
                 }, 1000);
-            }
-            else if (currentPageId == "#quiSommesNous") {
+            } else if (currentPageId == "#quiSommesNous") {
                 about.css({ transform: "scale(0)" });
-                setTimeout(function () {
+                setTimeout(function() {
                     about.removeClass("display-flex").addClass("display-none");
                 }, 1000);
             }
 
-            setTimeout(function () {
+            setTimeout(function() {
                 contact.css({ right: "100%" });
                 contact.css({ transform: "rotate(-50deg)" });
                 contact.removeClass("display-none");
                 contact.addClass("row");
             }, 1000);
-            setTimeout(function () {
+            setTimeout(function() {
                 contact.css({ right: "0" });
                 contact.css({ transform: "rotate(0)" });
             }, 1100);
@@ -219,7 +205,7 @@ $(document).ready(function () {
     }
 
     /*lien entre produits et page contact*/
-    $('.lq_link-more').click(function () {
+    $('.lq_link-more').click(function() {
         var value = $(this).attr("name");
         $('#productValue').val(value);
 
@@ -252,57 +238,88 @@ $(document).ready(function () {
     });
 
     /*Appel des notifications + réinitialisation du formulaire*/
-    $('#send').click(function (e) {
+    $('#send').click(function(e) {
         e.preventDefault();
         $('#contactForm').trigger('reset');
         $("#successText").text('Message bien envoyé !');
         $("#successNotification").jqxNotification("open");
     });
 
-    $('#return').click(function () {
+    $('#return').click(function() {
         $('#contactForm').trigger('reset');
         $("#errorText").text("Message non envoyé");
         $("#errorNotification").jqxNotification("open");
     });
 
     /*dark mode*/
-    $('.ls_checkbox').on('change', function () {
+    $('.ls_checkbox').on('change', function() {
         var is_it_checked = $(this).prop("checked"); //check the checkbox is checked or not
         if (is_it_checked == true) {
             $('.lq_product-details, .navbar, .form-control, .modal-content').css('background-color', '#3a3a3a');
             $('.navbar a, .navbar h2, #contact2 h2, #ls_contact-modal h2').css('color', 'white');
             $('#quiSommesNous, #quiSommesNous h2, #quiSommesNous h3').css('color', 'white');
-            $('#boutique h2, #boutique, #boutique a').css('color', 'white');
-            $('.td_home-slider__slide--1,.td_home-slider__slide--2,.td_home-slider__slide--3, #contact2 img').css('filter', 'grayscale(1)')
+            $('#boutique h2, #boutique, #boutique a, #contact h2, #inscriptionNewL h2').css('color', 'white');
+            $('.td_home-slider__slide--1,.td_home-slider__slide--2,.td_home-slider__slide--3, #contact2 img, .lq_cover').css('filter', 'grayscale(1)');
             $(document.body, '.navbar').css('background-color', 'var(--noir)');
         } else {
             $('.lq_product-details, .navbar, .form-control, .modal-content').removeAttr('style');
-            $('.navbar').removeAttr('style');
             $('.navbar a, .navbar h2, #contact2 h2, #ls_contact-modal h2').removeAttr('style');
             $('#quiSommesNous, #quiSommesNous h2, #quiSommesNous h3').removeAttr('style');
-            $('#boutique h2, #boutique, #boutique a').removeAttr('style');
-            $('.td_home-slider__slide--1,.td_home-slider__slide--2,.td_home-slider__slide--3').removeAttr('style');
-            $(document.body).removeAttr('style');
+            $('#boutique h2, #boutique, #boutique a, #contact h2, #inscriptionNewL h2').removeAttr('style');
+            $('.td_home-slider__slide--1,.td_home-slider__slide--2,.td_home-slider__slide--3, #contact2 img, .lq_cover').removeAttr('style');
+            $(document.body, '.navbar').removeAttr('style');
+
         };
     });
 
-    $('#validInscription').click(function () {
+    $('#validInscription').click(function() {
         $("#successText").text('Inscription effectuée');
         $("#successNotification").jqxNotification("open");
     });
 
 
     //Affichage lightbox
-    $(".lq_img").click(function () {
+    $(".lq_img").click(function() {
         var img = $(this).attr('src');
         var imgLightbox = img.replace("lq_img--petites", "lq_img--grandes");
         $(".lq_img--lightbox").html("<img src='" + imgLightbox + "'>");
         $(".lq_img--lightbox").fadeIn("slow").css("display", "flex");
     });
 
-    $(".lq_img--lightbox").click(function () {
+    $(".lq_img--lightbox").click(function() {
         $(".lq_img--lightbox").fadeOut("fast");
     });
 
+    /*modal*/
+    var j = 0;
+    if (j == 0) {
+        $("body").mouseleave(function() {
+            j = j + 1;
+            $('#inscriptionNewL').modal('show');
+            $('#annulerInscription, #validInscription').click(function() {
+                $('#inscriptionNewL').modal('hide');
+            });
+        });
+    }
+    /* changement monnaie*/
+    var dollar = $('#dollar');
+    var euro = $('#euro');
+    var yen = $('#yen');
+
+    $(dollar).click(function() {
+        $(".price1").replaceWith("<p class='title price1'>224,67$</p>");
+        $(".price2").replaceWith("<p class='title price2'>450,47$</p>");
+        $(".price3").replaceWith("<p class='title price3'>563,37$</p>");
+    });
+    $(euro).click(function() {
+        $(".price1").replaceWith("<p class='title price1'>199€</p>");
+        $(".price2").replaceWith("<p class='title price2'>399€</p>");
+        $(".price3").replaceWith("<p class='title price3'>499€</p>");
+    });
+    $(yen).click(function() {
+        $(".price1").replaceWith("<p class='title price1'>25617,34Y</p>");
+        $(".price2").replaceWith("<p class='title price2'>51363,41</p>");
+        $(".price3").replaceWith("<p class='title price3'>64236,45</p>");
+    });
 
 });
